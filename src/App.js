32fb5b6e648ca813dbf12e74f218c3496f3c6064
Kyle Barnett <kbarnett23@gmail.com>
2019-08-17 10:26:02 -0600

    got characters to display

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import AppRouter from "./components/AppRouter.js";
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+      <CharacterList />
+      {/* <TabNav /> */}
+      {/* <AppRouter /> */}
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Header, Icon, Image, Card } from 'semantic-ui-react';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <Image src={props.character.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.character.name}</Card.Header>
+        <Card.Meta>{props.character.species}</Card.Meta>
+        <Card.Description>
+          {props.character.status}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          10 Friends
+  </a>
+      </Card.Content>
+    </Card>
+  )
 }
